41e8f54ca70e2c2b2749ea65a2ac42f725c4a94f
NoobNotFound <chaniru2007@outlook.com>
2022-12-18 13:43:45 +0530

    updatw

--- NoobNotFound.WinUI.Common/Helpers/Extentions.cs
+++ NoobNotFound.WinUI.Common/Helpers/Extentions.cs
@@ -27,5 +27,17 @@ namespace NoobNotFound.WinUI.Common.Helpers
 
             return itemsToRemove.Count;
         }
+        public static int Week(this DateTime date)
+        {
+            date = date.Date;
+            DateTime firstMonthDay = new DateTime(date.Year, date.Month, 1);
+            DateTime firstMonthMonday = firstMonthDay.AddDays((DayOfWeek.Monday + 7 - firstMonthDay.DayOfWeek) % 7);
+            if (firstMonthMonday > date)
+            {
+                firstMonthDay = firstMonthDay.AddMonths(-1);
+                firstMonthMonday = firstMonthDay.AddDays((DayOfWeek.Monday + 7 - firstMonthDay.DayOfWeek) % 7);
+            }
+            return (date - firstMonthMonday).Days / 7 + 1;
+        }
     }
 }
